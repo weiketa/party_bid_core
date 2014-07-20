3fa548fbcefec4a415e6eb0396bd70ced02a9d74
weiketa <lyvoctor@gmail.com>
2014-07-20 11:57:44 +0800

    pass user has signed up can bid successfully when it is bidding test

---
+++ data_structure_1/src/bid.js
@@ -0,0 +1,6 @@
+function create_new_bid(activity_name){
+    var activities=JSON.parse(localStorage.activities);
+    _.findWhere(activities,{name:activity_name}).bids.push({name:'竞价1',biddings:[]});
+    localStorage.setItem('activities',JSON.stringify(activities))
+    console.log(_.findWhere(activities,{name:activity_name}))
+}
--- data_structure_1/src/notify_sms_received.js
+++ data_structure_1/src/notify_sms_received.js
@@ -1,21 +1,44 @@
 function notify_sms_received(sms_json){
     if(localStorage.is_signing_up=='true'&&!check_apply_message_repeat(sms_json)){
-        var apply_name=sms_json.messages[0].message.substr(2);
-        var apply_phone=sms_json.messages[0].phone;
-        var apply_message={name:apply_name,phone:apply_phone};
+        var apply_message=get_sign_up_message_data(sms_json);
         var activities=JSON.parse(localStorage.activities);
         activities[1].sign_ups.push(apply_message);
         localStorage.setItem('activities',JSON.stringify(activities));
         return;
+    }
+    if(localStorage.is_bidding=='true'){
+        var current_activity=localStorage.current_activity;
+        var current_bid=localStorage.current_bid;
+        var activities=JSON.parse(localStorage.activities);
+        var bids=_.findWhere(activities,{name:current_activity}).bids;
+        var biddings= _.findWhere(bids,{name:current_bid}).biddings;
+        var sign_ups=_.findWhere(activities,{name:current_activity}).sign_ups;
+        var bid_message_data=get_bid_message_data(sms_json);
+        bid_message_data.name= (_.findWhere(sign_ups,{phone:bid_message_data.phone})).name;
+        biddings.push(bid_message_data);
+        localStorage.setItem('activities',JSON.stringify(activities));
+
     }
 
 }
 function check_apply_message_repeat(sms_json){
     var apply_phone=sms_json.messages[0].phone;
     var activities=JSON.parse(localStorage.activities);
-    var current_activity=_.findWhere(activities,{name:'second activity'})
+    var current_activity=_.findWhere(activities,{name:'second activity'});
     if(_.find(current_activity.sign_ups,function(sign_up){return sign_up.phone==apply_phone}))
         return true;
     return false;
 
+}
+function get_sign_up_message_data(sms_json) {
+    var apply_name=sms_json.messages[0].message.substr(2);
+    var apply_phone=sms_json.messages[0].phone;
+    var apply_message={name:apply_name,phone:apply_phone};
+    return apply_message;
+}
+function get_bid_message_data(sms_json) {
+    var bid_price=sms_json.messages[0].message.substr(2);
+    var bid_phone=sms_json.messages[0].phone;
+    var bid_message={price:bid_price,phone:bid_phone};
+    return bid_message;
 }
