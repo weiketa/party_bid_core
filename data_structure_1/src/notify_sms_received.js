915bd888a5c3d4ae6676770890a441220ac25e6f
weiketa <lyvoctor@gmail.com>
2014-07-20 16:43:42 +0800

    pass should accept bid once test

--- data_structure_1/src/notify_sms_received.js
+++ data_structure_1/src/notify_sms_received.js
@@ -6,7 +6,7 @@ function notify_sms_received(sms_json){
         localStorage.setItem('activities',JSON.stringify(activities));
         return;
     }
-    if(localStorage.is_bidding=='true'){
+    if(localStorage.is_bidding=='true'&&is_signed_up(sms_json)&&!check_bid_message_repeat(sms_json)){
         var current_activity=localStorage.current_activity;
         var current_bid=localStorage.current_bid;
         var activities=JSON.parse(localStorage.activities);
@@ -25,10 +25,10 @@ function check_apply_message_repeat(sms_json){
     var apply_phone=sms_json.messages[0].phone;
     var activities=JSON.parse(localStorage.activities);
     var current_activity=_.findWhere(activities,{name:'second activity'});
-    if(_.find(current_activity.sign_ups,function(sign_up){return sign_up.phone==apply_phone}))
+    if(_.find(current_activity.sign_ups,function(sign_up){return sign_up.phone==apply_phone})){
         return true;
+    }
     return false;
-
 }
 function get_sign_up_message_data(sms_json) {
     var apply_name=sms_json.messages[0].message.substr(2);
@@ -41,4 +41,26 @@ function get_bid_message_data(sms_json) {
     var bid_phone=sms_json.messages[0].phone;
     var bid_message={price:bid_price,phone:bid_phone};
     return bid_message;
+}
+function is_signed_up(sms_json) {
+    var activities=JSON.parse(localStorage.activities);
+    var current_activity=localStorage.current_activity;
+    var sign_ups=_.findWhere(activities,{name:current_activity}).sign_ups;
+    var bid_message_data=get_bid_message_data(sms_json);
+    if(_.findWhere(sign_ups,{phone:bid_message_data.phone})){
+        return true;
+    }
+    return false;
+}
+function check_bid_message_repeat(sms_json){
+    var activities=JSON.parse(localStorage.activities);
+    var current_activity=localStorage.current_activity;
+    var current_bid=localStorage.current_bid;
+    var bids=_.findWhere(activities,{name:current_activity}).bids;
+    var biddings= _.findWhere(bids,{name:current_bid}).biddings;
+    var bid_message_data=get_bid_message_data(sms_json);
+    if(_.findWhere(biddings,{phone:bid_message_data.phone})){
+        return true;
+    }
+    return false;
 }
---
+++ data_structure_1/src/render.js
@@ -0,0 +1,3 @@
+transform_bids_to_view_model(current_activity){
+
+}
